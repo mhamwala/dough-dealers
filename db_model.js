bcabb60df79b2168c5753b23c5d7d5c5505ad7b8
Eduards Gzibovskis <gzibovskis1998@gmail.com>
2021-03-31 14:28:28 +0100

    Altered Post Method for '/addProduct' to be used in Postman via req.body

--- db_model.js
+++ db_model.js
@@ -1,39 +1,39 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'testuser',
-    host: 'localhost',
-    database: 'dddatabase',
-    password: 'root',
-    port: 5432
+  user: 'testuser',
+  host: 'localhost',
+  database: 'dddatabase',
+  password: 'root',
+  port: 5432
 });
 
 // Get data from Products table
 const getProducts = () => {
-    return new Promise(function(resolve, reject) {
-        pool.query('SELECT * FROM Products ORDER by id', (error, results) => {
-            if(error){
-                console.log('Failed to get products');
-                reject(error)
-            }
-            resolve(results.rows);
-        })
-    })
+  return new Promise(function(resolve, reject) {
+      pool.query('SELECT * FROM Products ORDER by id', (error, results) => {
+          if(error){
+              console.log('Failed to get products');
+              reject(error)
+          }
+          resolve(results.rows);
+      })
+  })
 }
 
 // Create product, sample data below.. will need to change to 'body'
-const createProduct = (product_name, stock, base_price) => {
-    return new Promise(function(resolve, reject) {
-      pool.query('INSERT INTO Products (Product_Name, Stock, Base_Price) VALUES ($1, $2, $3) RETURNING *', [product_name, stock, base_price], (error, results) => {
-        if (error) {
-          reject(error)
-        }
-        resolve(`A new product has been added added: ${JSON.stringify(results)}`)
-      })
+const createProduct = (productName, stock, basePrice) => {
+  return new Promise(function (resolve, reject) {
+    pool.query('INSERT INTO Products (Product_Name, Stock, Base_Price) VALUES ($1, $2, $3) RETURNING *', [productName, stock, basePrice], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`A new product has been added added: ${JSON.stringify(results.rows)}`)
     })
-  }
+  })
+}
 
 module.exports = {
-    getProducts,
-    createProduct
+  getProducts,
+  createProduct
 }
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const prompt = require('prompt-sync')()
+const express = require('express');
+const bodyParser = require('body-parser');
+const db_model = require('./db_model');
+const port = 3000;
 
-const db_model = require('./db_model')
+const app = express();
 
-app.use(express.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Alloq-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
@@ -27,22 +32,25 @@ app.get('/', (req, res) => {
 })
 
 // Add Data
-app.post('/addProduct', (req, res) => {
+app.post('/addProduct', async (req, res) => {
 
-    const product_name = prompt("Enter product name - ");
-    const stock = prompt("Enter stock value - ");
-    const base_price = prompt("Enter Base Price - ");
+    // Get Info
+    const {productName, stock, basePrice} = req.body;
 
-    db_model.createProduct(product_name, stock, base_price)
-    .then(response => {
-      res.status(200).send(response);
-    })
-    .catch(error => {
-      res.status(500).send(error);
-    })
+    try {
+
+        // Create product
+        let product = await db_model.createProduct(productName, stock, basePrice);
+
+        console.log(product);
+        res.status(200).send(product);
+        
+    } catch(error) {
+        
+        console.log(error);
+        res.status(500).send(error);
+    }
   })
-
-
 
 app.listen(port, () => {
     console.log(`App Running on port ${port}.`)
